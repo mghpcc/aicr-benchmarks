abf970286f8b79371154fb8000d93cf62592c2b6
Brian Gregor <bgregor@bu.edu>
2026-05-21 12:50:38 -0400

    multimode script fixed

--- Benchmark_WG/nvidia-hpcg/b200_multinode.sh
+++ Benchmark_WG/nvidia-hpcg/b200_multinode.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+# using 1 GPU per node across multiple nodes
+
 GPU=b200
-for n in 1 2 3 4 ; do
-    sbatch \
+for n in 2 4 8 ; do
+   sbatch \
         --partition=${GPU}-batch \
-        --gpus=4 \
-        --ntasks-per-node=4 \
-        --nodes=${n}
+        --gpus=${n} \
+        --ntasks-per-node=1 \
+        --nodes=${n} \
         --cpus-per-task=16 \
         --time=01:00:00 \
         --mem=200GB \
         --gpu-bind=closest \
-        --output=logs/hpcg_{$n}_node_4_gpu_${GPU}_%j.out \
-        --error=logs/hpcg_{$n}_node_4_gpu_${GPU}_%j.err \
-        --job-name=hpcg_${n}gpu ./aicr_hpcg.sh
+        --output=logs/hpcg_${n}_node_${n}_gpu_${GPU}_%j.out \
+        --error=logs/hpcg_${n}_node_${n}_gpu_${GPU}_%j.err \
+        --job-name=hpcg_${n}gpu_mn ./aicr_hpcg.sh
 done
